bca758da40215eaf4883580566d8d67ff649793c
liu.renjun <liu.renjun@mydreamplus.com>
2018-09-16 16:16:32 +0800

    change

--- go/src/app/Uploader.go
+++ go/src/app/Uploader.go
@@ -10,8 +10,9 @@ import (
 func main() {
 	url := "http://127.0.0.1:8088/api/upload"
 	
-	byte, _ := ioutil.ReadFile("1-1.txt")
-	req, _ := http.NewRequest("POST", url, bytes.NewReader(byte))
+	byte, _ := ioutil.ReadFile("1.txt")
+	
+	req, _ := http.NewRequest("POST", url, bytes.NewReader(byte[0:18]))
 	req.Header.Add("Host", "127.0.0.1:8088")
 	req.Header.Add("Content-Length", "18")
 	req.Header.Add("Content-Disposition", "attachment; filename=\"1111.txt\"")
@@ -25,8 +26,7 @@ func main() {
 	r, _ := ioutil.ReadAll(rep.Body)
 	fmt.Println(string(r))
 	
-	byte, _ = ioutil.ReadFile("1-2.txt")
-	req, _ = http.NewRequest("POST", url, bytes.NewReader(byte))
+	req, _ = http.NewRequest("POST", url, bytes.NewReader(byte[18:]))
 	req.Header.Add("Host", "127.0.0.1:8088")
 	req.Header.Add("Content-Length", "33")
 	req.Header.Add("Content-Disposition", "attachment; filename=\"1111.txt\"")
